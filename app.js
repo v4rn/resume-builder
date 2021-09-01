0b8c483e2d37ed80180c657f151e4ebc76a3d949
var <>
2021-09-01 18:06:10 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,101 @@
+document.body.onload = addResumeData;
+
+function addResumeData() {
+
+    /*
+        <header>
+            <h1>Varun Dabas</h1>
+            <h6>Minneapolis, MN</h6>
+            <h6> varun.dabas3@gmail.com</h6>
+        </header>
+
+        <div class="container">
+            <div class="dates">
+                <div>03/2020 -<br> 03/2018</div>
+            </div>
+            <div class="info">
+                <div>Recurse Center</div>
+                <div>
+                    <ul>
+                        <li>
+                            Officia nostrud duis culpa excepteur.
+                        </li>
+                        <li>
+                            Nostrud reprehenderit qui eu eiusmod Lorem laborum elit consequat dolor.
+                        </li>
+                        <li>
+                            Officia laboris nostrud nulla aliqua ullamco in. 
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+    */
+
+    // header data
+    const page = document.getElementById('page');
+    const header = document.createElement('header');
+
+    const name = document.createElement('h1');
+    name.textContent = data.name; 
+
+    const city_email_div = document.createElement('div');
+    city_email_div.className = 'city_email'
+    city_email_div.innerHTML = `<span>${data.city}</span><span>${data.email}</span>`
+
+    header.appendChild(name)
+    header.appendChild(city_email_div)
+
+    page.appendChild(header);
+
+    // resume data
+    data.jobs.forEach(job => {
+        // CONTAINER DIV which contains DATE AND INFO DIV
+        const container_div = document.createElement('div');
+        container_div.className = 'container';
+
+        // DATE DIV
+        const dates_div = document.createElement('div');
+        dates_div.className = 'dates';
+        const dates_text = document.createElement('span');
+        dates_text.textContent = `${job.date}`;
+        dates_div.appendChild(dates_text);
+        container_div.appendChild(dates_div);
+
+        // INFO DIV
+        let info_div = document.createElement('div');
+        info_div.className = 'info';
+
+        // INFO DIV - title 
+        const title_div = document.createElement('div');
+        title_div.className = 'info_title';
+
+        const job_title_span = document.createElement('span');
+        job_title_span.className = 'job_title';
+
+        const tech_used_span = document.createElement('span');
+        tech_used_span.className = 'tech_used';
+
+        job_title_span.innerHTML = `${job.title} - <i>${job.summary}</i>`;
+        tech_used_span.innerHTML = `${job.tech_used}`
+
+        title_div.appendChild(job_title_span)
+        title_div.appendChild(tech_used_span)
+        info_div.appendChild(title_div)
+
+        // INFO DIV - details
+        const details_div = document.createElement('div');
+        const bullets_ul = document.createElement('ul');
+        job.details.forEach(detail => {
+            let detail_li = document.createElement('li');
+            detail_li.textContent = detail
+            bullets_ul.appendChild(detail_li)            
+        })
+        details_div.appendChild(bullets_ul); // add bullets to details div tag
+        info_div.appendChild(details_div);   // add details div to info div
+
+        container_div.appendChild(info_div); // add info div to container
+        page.appendChild(container_div);     // add container to page
+    })
+}
---
+++ data.js
@@ -0,0 +1,77 @@
+data = {
+    
+    name: 'Varun Dabas',
+    city: 'Minneapolis, MN',
+    email: 'varun.dabas3@gmail.com',
+    jobs: [
+        {   // Recurse Center
+            date: 'May 2021',
+            title: 'Recurse Center',
+            summary: 'A self-directed, educational retreat for programmers',
+            tech_used: 'JavaScript, NodeJS, Express, Python, CSS, MongoDB',
+            details: [
+                "Reimmersed in software development, and explored variety of topics including Full Stack Development, Complexity Theory, Graphics and Asynchronous programming",
+                "Built a web application written in NodeJS, Express and JavaScript that shows current and past Recursers' locations on Google maps",
+                "Created a prototype of a resume builder app in HTML, CSS and JavaScript that takes input data from a JSON file",
+                "Simulated Conway's Game of Life and Rule 30 cellular automaton in HTML Canvas and JavaScript",
+                "Learned about MongoDB by following M001 MongoDB course from MongoDB University",
+                "Experimented with promises, generators and async/await to make batched asynchronous requests in Javascript",
+                "Wrote an HTTP Server in Python from scratch which interacted with a small in-memory database (dictionary)",
+                "Built a command line Sudoku solver in Python",
+            ],
+        },
+        {   // Career Break
+            date: 'Aug 2020',
+            title: 'Sabbatical Year',
+            summary: 'Career break from the industry',
+            tech_used: '',
+            details: [
+                "Left Micron Technology, Inc. to explore interests in Psychology, Physics and Mathematics", 
+                "Particularly interested in Human Behavior and the process of Problem Solving",
+            ],
+        },
+        {   // Micron Technology, Inc.
+            date: 'Mar 2013',
+            title: 'Micron Technology, Inc.',
+            summary: 'Product Engineer',
+            tech_used: 'Python, C++, Linux, Bash, SQL, GNU Debugger, Git',
+            details: [
+                "Responsible to develop software to study NAND memory functionality and reliability",
+                "Analyzed memory footprint of a data structure, optimized it and reduced the memory footprint by a factor of 100",
+                "Worked with memory corruptions that involved understanding of GIL, memory fragmentation, operating system and gdb",
+                "Built standalone programs to analyze, gather and present data in C++ and Python that used concepts including - socket programming, Linux signals, multi-processing, multi-threading, Linux command line programs",
+                "Built daemon process from scratch in python to asynchronously transfer data. Managed the daemon by building a program that started, stopped and check the status of the daemons",
+                "Developed GUIs in wxPython",
+                "Conducted phone screen and on-site technical interview process",
+                "Trained/Consulted team members on programming languages and operating system queries",
+                "Enrolled in CS140, an Operating Systems course from Stanford University. The course involved projects to implement a priority thread schedular, system calls, paging, and an indexed file system",
+                "Researched/studied libraries/programs including CPython internals, OpenMP, OpenMPI, Valgrind, memory allocators to find techniques to further optimize existing programs"
+            ],
+        },
+        {   // Packet Digital
+            date: 'May 2012',
+            title: 'Packet Digital',
+            summary: 'Engineering Intern',
+            tech_used: 'C, Python, SystemVerilog',
+            details: [
+                "Interned across hardware, software and digital design teams, and helped with scripting and troubleshooting",
+                "Learned C, Python and SystemVerilog by reading existing programs",
+                "Implemented and tested UART communication protocol in SystemVerilog",
+                "Designed digital clocks in SystemVerilog for a given frequency on FPGA",
+            ],
+        },
+        {   // NDSU
+            date: 'Dec 2012',
+            title: 'North Dakota State University',
+            summary: 'BS, Electrical Engineering',
+            tech_used:'GPA: 3.69/4.0',
+            details: [
+                "Completed a wide breadth of courses in digital logic and communication systems",
+                "Worked as a Teaching Assistant to a Circuit Analysis course",
+                "Worked as a Tutor in Mathematics, Physics and Chemistry",
+            ]
+        }, 
+        
+    ],
+}
+
